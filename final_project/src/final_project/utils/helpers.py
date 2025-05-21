a128f01f5a34ddef34cca49c5f86e9c14a0a2e89
Aiman <aimann@stud.ntnu.no>
2025-05-21 16:08:02 +0200

    better config

--- final_project/src/final_project/utils/helpers.py
+++ final_project/src/final_project/utils/helpers.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+import random
+import numpy as np
 import torch
+import yaml
 
 from final_project.models.vae import VAE
 
@@ -14,7 +17,25 @@ def load_model(model_type, model_path, device):
     if not Path(model_path).exists():
         raise FileNotFoundError(f"Model path {model_path} does not exist.")
     if model_type == 'vae':
-        model = VAE(hidden_dim=64, embedding_dim=8)
+        vae_config = load_config("src/final_project/configs/vae_config.yaml")
+        hidden_dim = vae_config["model"]["hidden_dim"]
+        embedding_dim = vae_config["model"]["embedding_dim"]
+        model = VAE(hidden_dim=hidden_dim, embedding_dim=embedding_dim)
         model.load_state_dict(torch.load(model_path, map_location=device))
+        model.to(device)
     print(f"Model loaded from {model_path}")
     return model
+
+def load_config(path):
+    with open(path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+def set_seed(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.deterministic = False
