7ad054db0619d393e9624c113831d181cf19fff8
Aiman <aimann@stud.ntnu.no>
2025-05-27 14:05:33 +0200

    small fixes

--- final_project/src/final_project/utils/__init__.py
+++ final_project/src/final_project/utils/__init__.py
@@ -1,1 +1,2 @@
-from .helpers import save_model, load_model, load_config, set_seed, extract_latents
+from .helpers import save_model, load_config, set_seed
+from .latent_extraction import flatten_latents
